0381e6aa3be9089daf2857d1b12f4db7704bfb5b
liz rush <liz@lizmrush.com>
2018-06-12 10:48:47 -0700

    Remove theme locally

--- themes/seattleagainstslavery-0.9.1/page-templates/volunteer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Template Name: Volunteer
-*/
-get_header(); ?>
-
-<?php get_template_part('template-parts/featured-image', 'volunteer'); ?>
-<?php get_template_part('template-parts/flexible', 'positions'); ?>
-<?php get_footer();
