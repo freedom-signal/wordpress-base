a0bcbe83c14ccf60d999cbddaaf823d7095149db
liz rush <liz@lizmrush.com>
2018-07-09 18:25:48 -0700

    add set -e and set -x to docker/command.sh

--- docker/command.sh
+++ docker/command.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 until nc -z -v -w30 db 3306
 do
   echo "Waiting for database connection..."
@@ -11,8 +14,8 @@ composer wordpress-setup-core-install -- \
   --admin_user=admin \
   --admin_password=admin \
   --admin_email=admin@example.com \
-  --url="http://localhost/"
+  --url="http://localhost/" || true
 
-composer wordpress-setup-finalize
+composer wordpress-setup-finalize || true
 
 vendor/bin/heroku-php-apache2 -C apache2-wordpress.conf wordpress/
